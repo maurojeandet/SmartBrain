4c8bee7c3c3ef79792c7c8d3c7a613cb457efb58
Mauro Jeandet <mauro.jeandet@southworks.com>
2021-01-29 16:03:24 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+    </div>
+  );
+}
+ 
+export default App;
